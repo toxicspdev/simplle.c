0bd9e88917c6e7768a4e1ccf44fc733bd6a51422
toxicspdev <PRAVINWARULE2@GMAIL.COM>
2024-07-19 06:42:14 +0000

    Done

---
+++ first.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("enter two no");
+    scanf("%d %d",&a,&b);
+    printf("choose two no");
+    scanf("%d",&c);
+    printf("1: add:");
+    case 1:
+    c= a+b;
+    printf("add two no %d",(a+b);
+}
